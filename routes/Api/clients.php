155581abcd8102f7bea6b926bcbf429984462c62
xiaoxiong <2098634557@qq.com>
2019-10-11 18:47:04 +0800

    更新

--- routes/Api/clients.php
+++ routes/Api/clients.php
@@ -164,10 +164,10 @@ Route::group([
 	     *          )
 	     *     ),
 	     *      @OA\Response(response=200,description="成功",@OA\MediaType(mediaType="application/json"),),
-	     *     @OA\Response(response="401",description="未登录",),
-	     *     @OA\Response(response="403",description="未授权",),
+	     *      @OA\Response(response="401",description="未登录",),
+	     *      @OA\Response(response="403",description="未授权",),
 	     *      @OA\Response(response=422,description="验证失败信息"),
-	     *     @OA\Response(response=500,description="系统错误"),
+	     *      @OA\Response(response=500,description="系统错误"),
 	     * )
 	     */
 	    Route::delete('delete', [
